e309d7e072b21b869b8a2dad757d110b76af9d8d
warrentc3 <warren@carrigan.email>
2017-11-10 04:23:04 -0500

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,24 +1,33 @@
 #!/bin/csh
-set w1 = `jot -r 1 4 20`
-set w2 = `jot -r 1 5 30`
-set w3 = `jot -r 1 5 30`
-sleep $w1
+set w1 = `jot -r 1 2 15`
 set filename = `printf "%s " $1:t:r:r`
 set grab = `printf "%s " $1:h`
 set a = `echo $filename.ts`
-set b = `echo "$grab/$filename"--cut.ts`
+set b = `echo "$grab/$filename"_cut.ts`
 set c = `echo "$grab/$filename".mp4`
-cp "$1" /media/PlexDVR/.plexignore/"$a"
+sleep $w1
+
+if ( -e "$b" ) then
+echo "Comcut already done"
+endif
+
 while (1)
-        if (-e "/tmp/comchap.lock") then
-                sleep $w2
-        else if (`pgrep HandBrakeCLI`) then
-                sleep $w3
-        else
-                comcut --ffmpeg=/usr/bin/ffmpeg --comskip=/usr/bin/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/media/Comskip/comskip.ini "$1" "$b"
-                HandBrakeCLI --input "$b" --output "$c"  --preset-import-file /media/comchap/hb-dvr.json
-                rm "$b"
-                break
-        endif
+if ( ! -e "$b" ) then
+echo "Running Comcut"
+comcut --ffmpeg=/usr/bin/ffmpeg --comskip=/usr/bin/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/media/Comskip/comskip.ini "$1" "$b"
+echo "Comcut complete"
+endif
+break
 end
-exit
+
+while (1)
+if (! `pgrep HandBrakeCLI` ) then
+break
+endif
+echo "HandBrakeCLI is still running. Will try again in a minute."
+sleep 60
+end
+
+HandBrakeCLI --input "$b" --output "$c" --preset-import-file /media/comchap/hb-dvr.json
+
+rm $b
